e6984728da9b4958f74a5cf62966cd5d80baa58d
williamstruman <fanxiao720@gmail.com>
2017-10-04 14:53:49 -0400

    Add files via upload

---
+++ LSTM_soap.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 10:39:57 2017
+
+@author: rbao
+"""
+
+"""
+# load and plot dataset
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from datetime import datetime
+# load dataset
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+
+
+# define variable
+train_row = 24
+test_row = 36
+
+
+
+series = read_csv('sales_shampoo.csv', header=0, parse_dates=[0]) # , index_col=0, squeeze=True, date_parser=parser)
+# summarize first few rows
+print(series.head())
+# line plot
+# series.plot()
+# pyplot.show()
+
+# split data into train and test
+X = series.values
+train, test = X[0:train_row], X[train_row:test_row]
+
+# walk-forward validation
+history = [x for x in train]
+predictions = list()
+for i in range(len(test)):
+	# make prediction
+	predictions.append(history[-1])
+	# observation
+	history.append(test[i])
+# report performance
+rmse = sqrt(mean_squared_error(test, predictions))
+print('RMSE: %.3f' % rmse)
+# line plot of observed vs predicted
+pyplot.plot(test)
+pyplot.plot(predictions)
+pyplot.show()
+
+"""
+
+
+
+
+
+
+
+
+from pandas import DataFrame
+from pandas import Series
+from pandas import concat
+from pandas import read_csv
+from pandas import datetime
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from math import sqrt
+from matplotlib import pyplot
+import numpy
+
+# date-time parsing function for loading the dataset
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+
+# frame a sequence as a supervised learning problem
+def timeseries_to_supervised(data, lag=1):
+	df = DataFrame(data)
+	columns = [df.shift(i) for i in range(1, lag+1)]
+	columns.append(df)
+	df = concat(columns, axis=1)
+	df.fillna(0, inplace=True)
+	return df
+
+# create a differenced series
+def difference(dataset, interval):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return Series(diff)
+
+# invert differenced value
+def inverse_difference(history, yhat, interval=1):
+	return yhat + history[-interval]
+
+# scale train and test data to [-1, 1]
+def scale(train, test):
+	# fit scaler
+	scaler = MinMaxScaler(feature_range=(-1, 1))
+	scaler = scaler.fit(train)
+	# transform train
+	train = train.reshape(train.shape[0], train.shape[1])
+	train_scaled = scaler.transform(train)
+	# transform test
+	test = test.reshape(test.shape[0], test.shape[1])
+	test_scaled = scaler.transform(test)
+	return scaler, train_scaled, test_scaled
+
+# inverse scaling for a forecasted value
+def invert_scale(scaler, X, value):
+	new_row = [x for x in X] + [value]
+	array = numpy.array(new_row)
+	array = array.reshape(1, len(array))
+	inverted = scaler.inverse_transform(array)
+	return inverted[0, -1]
+
+# fit an LSTM network to training data
+def fit_lstm(train, batch_size, nb_epoch, neurons):
+	X, y = train[:, 0:-1], train[:, -1]
+	X = X.reshape(X.shape[0], 1, X.shape[1])
+	model = Sequential()
+	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+	model.add(Dense(1))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	for i in range(nb_epoch):
+		model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+		model.reset_states()
+	return model
+
+# make a one-step forecast
+def forecast_lstm(model, batch_size, X):
+	X = X.reshape(1, 1, len(X))
+	yhat = model.predict(X, batch_size=batch_size)
+	return yhat[0,0]
+
+# load dataset
+series = read_csv('sales_shampoo.csv', header=0, parse_dates=[0]) #, index_col=0, squeeze=True, date_parser=parser)
+
+# transform data to be stationary
+raw_values = series.values[:36]
+diff_values = difference(raw_values, 1)
+
+# transform data to be supervised learning
+supervised = timeseries_to_supervised(diff_values, 1)
+supervised_values = supervised.values
+
+# split data into train and test-sets
+train, test = supervised_values[0:-12], supervised_values[-12:]
+
+# transform the scale of the data
+scaler, train_scaled, test_scaled = scale(train, test)
+
+# fit the model
+lstm_model = fit_lstm(train_scaled, 1, 3000, 4)
+# forecast the entire training dataset to build up state for forecasting
+train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
+lstm_model.predict(train_reshaped, batch_size=1)
+
+# walk-forward validation on the test data
+predictions = list()
+for i in range(len(test_scaled)):
+	# make one-step forecast
+	X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
+	yhat = forecast_lstm(lstm_model, 1, X)
+	# invert scaling
+	yhat = invert_scale(scaler, X, yhat)
+	# invert differencing
+	yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
+	# store forecast
+	predictions.append(yhat)
+	expected = raw_values[len(train) + i + 1]
+	print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+
+# report performance
+rmse = sqrt(mean_squared_error(raw_values[-12:], predictions))
+print('Test RMSE: %.3f' % rmse)
+# line plot of observed vs predicted
+pyplot.plot(raw_values[-12:])
+pyplot.plot(predictions)
+pyplot.show()
+
+
+
+
+"""flight prediction"""
+
+
+
+
+# Multilayer Perceptron to Predict International Airline Passengers (t+1, given t)
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
+dataframe = pandas.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+# create and fit Multilayer Perceptron model
+model = Sequential()
+model.add(Dense(8, input_dim=look_back, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=200, batch_size=2, verbose=2)
+# Estimate model performance
+trainScore = model.evaluate(trainX, trainY, verbose=0)
+print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore, math.sqrt(trainScore)))
+testScore = model.evaluate(testX, testY, verbose=0)
+print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore, math.sqrt(testScore)))
+# generate predictions for training
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(dataset)
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
